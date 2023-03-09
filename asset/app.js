bd98fe8f9fcb39dcb44e5f46eb9badf729f23b3b
okekevicktur <okekevicktur@gmail.com>
2023-03-09 14:33:30 +0100

    Trendy Search update

--- asset/app.js
+++ asset/app.js
@@ -28,7 +28,7 @@ search.addEventListener('click', function(){
     // return;
     if (newMovie === "") {
         alert("type a movie title to search");
-        justTrendy.click();
+        fixTrend();
     }else{
             trendWrapDiv.style.display= "block";
             otherWrapDiv.style.display= "none";
@@ -342,7 +342,74 @@ BtnMovies.addEventListener('click', function(){
     }
     
 });
+function fixTrend(){
+    recommendTxt.style.display= "block";
+    mainHeader.innerText= "Trending";
+    highlight(justTrendy);
+    trendWrapDiv.style.display= "block";
+    otherWrapDiv.style.display= "none";
+    let alltrends = document.querySelectorAll('.trend');
+    alltrends.forEach(border =>{
+        border.remove();
+    });
+    getMoviesTrending().forEach(movie => {   
+        // Create a new trend element
+        const divTrend = document.createElement('div');
+        divTrend.className = "trend";
+        divTrend.style.backgroundImage = `url("/asset/${movie.thumbnail.regular.large}")`;
+        // url("./images/Victors\ Passport.JPG") no-repeat center center/cover;
+        // Create a new bookmark element
+        const divBookmark = document.createElement('div');
+        divBookmark.className = "bookmarkWrap";
+        // Create a new bookmark icon
+        const iconBookmark = document.createElement('i');
+        iconBookmark.classList.add('fa-solid', 'fa-bookmark', 'bookmrkVid');
 
+        // Create a new description container
+        const divdescription = document.createElement('div');
+        divdescription.className = "moviedescWrap";
+        // Create a new details container
+        const divdetails = document.createElement('div');
+        divdetails.className = "detailswrap";
+
+        // Create a new details container
+        const spanYear = document.createElement('span');
+        spanYear.className = "detailspan";
+        spanYear.innerText = `${movie.year}`;
+
+        const iconFilm = document.createElement('img');
+        const spanCtg = document.createElement('span');
+        spanCtg.className=  'detailspan';
+        if (`${movie.category}` === "Movie") {
+            iconFilm.src = "/asset/icon-category-movie.svg"; 
+            spanCtg.innerText = `${movie.category}`;
+        } else if(`${movie.category}` === "TV Series") {
+            
+            iconFilm.src = "/asset/icon-category-tv.svg"; 
+            spanCtg.innerText = `${movie.category}`;
+        }
+        const spanPg = document.createElement('span');
+        spanPg.className = "detailspan";
+        spanPg.innerText = `${movie.rating}`;
+
+        const movietitle = document.createElement('h3');
+        movietitle.innerText = `${movie.title}`;
+
+        //Append elements to containers
+        trendsContainer.appendChild(divTrend);
+        divTrend.appendChild(divBookmark);
+        divTrend.appendChild(divdescription);
+        divBookmark.appendChild(iconBookmark);
+        divdescription.appendChild(divdetails);
+        divdetails.appendChild(spanYear);
+        divdetails.appendChild(iconFilm);
+        divdetails.appendChild(spanCtg);
+        divdetails.appendChild(spanPg);
+        divdetails.appendChild(movietitle); 
+        // console.log(movie.thumbnail.regular.large);
+     });
+    notTrending(getMoviesNotTrending());
+}
 justTrendy.addEventListener('click', function(){
     if (trendWrapDiv.style.display == "block" &&   otherWrapDiv.style.display == "none") {
         return;
